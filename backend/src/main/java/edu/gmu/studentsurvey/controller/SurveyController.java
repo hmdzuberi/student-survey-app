bbc71dc2d4fc2c5337af780c52ced7cfc49e7b6b
Hamaad Zuberi <hamaadz@outlook.com>
2025-03-24 23:10:25 -0400

    initial commit

---
+++ backend/src/main/java/edu/gmu/studentsurvey/controller/SurveyController.java
@@ -0,0 +1,54 @@
+package edu.gmu.studentsurvey.controller;
+
+import edu.gmu.studentsurvey.model.Survey;
+import edu.gmu.studentsurvey.service.SurveyService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/surveys")
+@CrossOrigin(origins = "http://localhost:4200")
+public class SurveyController {
+
+    private final SurveyService surveyService;
+
+    @Autowired
+    public SurveyController(SurveyService surveyService) {
+        this.surveyService = surveyService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Survey>> getAllSurveys() {
+        List<Survey> surveys = surveyService.getAllSurveys();
+        return new ResponseEntity<>(surveys, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Survey> getSurveyById(@PathVariable Long id) {
+        Survey survey = surveyService.getSurveyById(id);
+        return new ResponseEntity<>(survey, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Survey> createSurvey(@Valid @RequestBody Survey survey) {
+        Survey newSurvey = surveyService.createSurvey(survey);
+        return new ResponseEntity<>(newSurvey, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Survey> updateSurvey(@PathVariable Long id, @Valid @RequestBody Survey survey) {
+        Survey updatedSurvey = surveyService.updateSurvey(id, survey);
+        return new ResponseEntity<>(updatedSurvey, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSurvey(@PathVariable Long id) {
+        surveyService.deleteSurvey(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ backend/src/main/java/edu/gmu/studentsurvey/service/SurveyService.java
@@ -0,0 +1,63 @@
+package edu.gmu.studentsurvey.service;
+
+import edu.gmu.studentsurvey.exception.ResourceNotFoundException;
+import edu.gmu.studentsurvey.model.Survey;
+import edu.gmu.studentsurvey.repository.SurveyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SurveyService {
+
+    private final SurveyRepository surveyRepository;
+
+    @Autowired
+    public SurveyService(SurveyRepository surveyRepository) {
+        this.surveyRepository = surveyRepository;
+    }
+
+    public List<Survey> getAllSurveys() {
+        return surveyRepository.findAll();
+    }
+
+    public Survey getSurveyById(Long id) {
+        return surveyRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Survey not found with id: " + id));
+    }
+
+    public Survey createSurvey(Survey survey) {
+        return surveyRepository.save(survey);
+    }
+
+    public Survey updateSurvey(Long id, Survey surveyDetails) {
+        Survey survey = getSurveyById(id);
+        
+        survey.setFirstName(surveyDetails.getFirstName());
+        survey.setLastName(surveyDetails.getLastName());
+        survey.setStreetAddress(surveyDetails.getStreetAddress());
+        survey.setCity(surveyDetails.getCity());
+        survey.setState(surveyDetails.getState());
+        survey.setZip(surveyDetails.getZip());
+        survey.setTelephone(surveyDetails.getTelephone());
+        survey.setEmail(surveyDetails.getEmail());
+        survey.setSurveyDate(surveyDetails.getSurveyDate());
+        survey.setLikedStudents(surveyDetails.isLikedStudents());
+        survey.setLikedLocation(surveyDetails.isLikedLocation());
+        survey.setLikedCampus(surveyDetails.isLikedCampus());
+        survey.setLikedAtmosphere(surveyDetails.isLikedAtmosphere());
+        survey.setLikedDormRooms(surveyDetails.isLikedDormRooms());
+        survey.setLikedSports(surveyDetails.isLikedSports());
+        survey.setInterestSource(surveyDetails.getInterestSource());
+        survey.setRecommendationLikelihood(surveyDetails.getRecommendationLikelihood());
+        survey.setAdditionalComments(surveyDetails.getAdditionalComments());
+        
+        return surveyRepository.save(survey);
+    }
+
+    public void deleteSurvey(Long id) {
+        Survey survey = getSurveyById(id);
+        surveyRepository.delete(survey);
+    }
+}
